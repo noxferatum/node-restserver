c3a76bda3e148d7738080eaf31d90480f899bf2a
Rodrigo <rodrigomelongutte@gmail.com>
2020-03-26 08:59:53 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,19 @@
 // ============================================
-// Puerto
+//   Puerto
 // ============================================
 process.env.PORT = process.env.PORT || 3000;
+// ============================================
+//   Entorno
+// ============================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+// ============================================
+//   Base de Datos
+// ============================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://noxferatum:iI5FODovWYjXiLS1@cluster0-x5fez.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
